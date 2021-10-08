19635000d5fcdf1b2b3e271294b568deb000e11e
Patricia Aas <patricia@turtlesec.no>
2021-10-08 12:22:05 +0200

    Set up basic vcpkg structure

---
+++ vcpkg_project/src/main.cpp
@@ -0,0 +1,18 @@
+#include <fmt/format.h>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    fmt::print("Hello {}", "vcpkg");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello vcpkg");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
