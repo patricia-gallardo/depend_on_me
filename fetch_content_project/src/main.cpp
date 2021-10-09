aa9122ba1418d6e7f651fc9779f46f45764d55e0
Patricia Aas <patricia@turtlesec.com>
2021-10-09 21:50:55 +0200

    Make a simple FetchContent example

---
+++ fetch_content_project/src/main.cpp
@@ -0,0 +1,18 @@
+#include <fmt/format.h>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    fmt::print("Hello {}", "FetchContent");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Hello FetchContent");
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        window.display();
+    }
+    return EXIT_SUCCESS;
+}
