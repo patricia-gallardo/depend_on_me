256dd4d6596b61d93acebb4ad5f4f0c57f86e6c5
Patricia Aas <psmaas@gmail.com>
2021-10-20 19:35:05 +0200

    Move FetchContent into its own file

---
+++ fetch_content_project/setup_fetch_content.cmake
@@ -0,0 +1,15 @@
+include(FetchContent)
+FetchContent_Declare(Catch2
+        GIT_REPOSITORY https://github.com/catchorg/Catch2.git
+        GIT_TAG        v2.13.7)
+FetchContent_MakeAvailable(Catch2)
+
+FetchContent_Declare(fmt
+        GIT_REPOSITORY https://github.com/fmtlib/fmt.git
+        GIT_TAG 8.0.1)
+FetchContent_MakeAvailable(fmt)
+
+FetchContent_Declare(SFML
+        GIT_REPOSITORY https://github.com/SFML/SFML.git
+        GIT_TAG 2.5.1)
+FetchContent_MakeAvailable(SFML)
