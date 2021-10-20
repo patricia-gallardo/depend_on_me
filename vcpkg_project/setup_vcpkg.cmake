b03458b079222f12126e580d5295604288a8aec0
Patricia Aas <psmaas@gmail.com>
2021-10-20 19:09:29 +0200

    Pull the vcpkg code into its own file

---
+++ vcpkg_project/setup_vcpkg.cmake
@@ -0,0 +1,20 @@
+include(FetchContent)
+FetchContent_Declare(
+        vcpkg
+        GIT_REPOSITORY https://github.com/microsoft/vcpkg
+        GIT_TAG 5568f110b509a9fd90711978a7cb76bae75bb092 # 2021.05.12
+)
+
+if (NOT DEFINED CMAKE_TOOLCHAIN_FILE)
+    FetchContent_GetProperties(vcpkg
+            POPULATED vcpkg_POPULATED
+            SOURCE_DIR vcpkg_SOURCE_DIR)
+    if (NOT vcpkg_POPULATED)
+        FetchContent_Populate(vcpkg)
+    endif ()
+
+    set(CMAKE_TOOLCHAIN_FILE
+            "${vcpkg_SOURCE_DIR}/scripts/buildsystems/vcpkg.cmake")
+    #    set(VCPKG_CHAINLOAD_TOOLCHAIN_FILE
+    #            "${CMAKE_CURRENT_LIST_DIR}/../common/cmake/toolchain.cmake")
+endif ()
