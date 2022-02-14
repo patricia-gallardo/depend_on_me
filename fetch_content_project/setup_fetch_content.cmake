38f092bd29af56c4b602eaa2ae10b369aebcbc55
Riley Levy <rileylevy95@gmail.com>
2022-02-14 17:34:52 -0500

    Override find_package to ignore already fetched

--- fetch_content_project/setup_fetch_content.cmake
+++ fetch_content_project/setup_fetch_content.cmake
@@ -1,19 +1,29 @@
 include(FetchContent)
 
+set(handled_by_fetch_content "")
+
 FetchContent_Declare(Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG        v2.13.7)
 FetchContent_MakeAvailable(Catch2)
+list(APPEND handled_by_fetch_content Catch2)
 
 FetchContent_Declare(fmt
         GIT_REPOSITORY https://github.com/fmtlib/fmt.git
         GIT_TAG 8.0.1)
 FetchContent_MakeAvailable(fmt)
+list(APPEND handled_by_fetch_content fmt)
 
 FetchContent_Declare(SFML
         GIT_REPOSITORY https://github.com/SFML/SFML.git
         GIT_TAG 2.5.1)
 FetchContent_MakeAvailable(SFML)
+list(APPEND handled_by_fetch_content SFML)
 
-function(noop_find_package)
-endfunction()
+macro(find_package)
+        if(NOT "${ARGV0}" IN_LIST handled_by_fetch_content)
+                _find_package(${ARGV})
+        else()
+                message(DEBUG "${ARGV0} already fetched")
+        endif()
+endmacro()
