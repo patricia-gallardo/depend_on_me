94bd4da2e827badb09726a3008fc33e7980e1eef
Riley Levy <rileylevy95@gmail.com>
2022-02-14 21:48:06 -0500

    Teach find_package to ignore pkgs in CPM_PACKAGES
    I believe CPM_PACKAGES lists the name of every package CPM is asked to
    add. So we can use it to avoid calls to find_package

--- cpm_project/setup_cpm.cmake
+++ cpm_project/setup_cpm.cmake
@@ -4,6 +4,21 @@ if (NOT EXISTS "${CMAKE_BINARY_DIR}/CPM.cmake")
          "${CMAKE_BINARY_DIR}/CPM.cmake")
 endif ()
 
+# Note: when you shadow a function, the previous definition
+# is still available by prefixing with an underscore
+
+# Idea from C++Now2017: Daniel Pfeifer "Effective CMake"
+# https://youtu.be/bsXLMQ6WgIk?t=3159
+# Note: there appears to be a typo in the presentation slides.
+# We need to check ${ARGV0}, not ${ARG0}.
+macro(find_package)
+        if(NOT "${ARGV0}" IN_LIST CPM_PACKAGES)
+                _find_package(${ARGV})
+        else()
+                message(DEBUG "${ARGV0} already added by CPM")
+        endif()
+endmacro()
+
 include(${CMAKE_BINARY_DIR}/CPM.cmake)
 CPMAddPackage("gh:catchorg/Catch2#v2.13.7")
 CPMAddPackage("gh:fmtlib/fmt#8.0.1")
