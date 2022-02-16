7a53a34f6d192f421f0ff04a5d6845d321f4bae6
riley levy <32276427+rileylev@users.noreply.github.com>
2022-02-16 13:24:46 -0500

    fetch_content and find_package: a workaround for compatibility (#7)
    * Stub function noop_find_package
    
    * Override find_package to ignore already fetched
    
    * Cite my sources with comments
    
    * Gather find_package support, use find_package Catch2
    
    * Remove noisy blank line
    
    * Change cmake vars to be UPPERCASE
    
    * Teach find_package to ignore pkgs in CPM_PACKAGES
    
    I believe CPM_PACKAGES lists the name of every package CPM is asked to
    add. So we can use it to avoid calls to find_package

--- fetch_content_project/setup_fetch_content.cmake
+++ fetch_content_project/setup_fetch_content.cmake
@@ -1,5 +1,28 @@
 include(FetchContent)
 
+# A hack that lets FetchContent cooperate with find_package
+set(HANDLED_BY_FETCH_CONTENT "") # a list of packages for find_package to ignore
+
+# Note: when you shadow a function, the previous definition
+# is still available by prefixing with an underscore
+
+# Idea from C++Now2017: Daniel Pfeifer "Effective CMake"
+# https://youtu.be/bsXLMQ6WgIk?t=3159
+# Note: there appears to be a typo in the presentation slides.
+# We need to check ${ARGV0}, not ${ARG0}.
+macro(find_package)
+        if(NOT "${ARGV0}" IN_LIST HANDLED_BY_FETCH_CONTENT)
+                _find_package(${ARGV})
+        else()
+                message(DEBUG "${ARGV0} already fetched")
+        endif()
+endmacro()
+
+macro(FetchContent_MakeAvailable)
+        list(APPEND HANDLED_BY_FETCH_CONTENT ${ARGV0})
+        _FetchContent_MakeAvailable(${ARGV})
+endmacro()
+
 FetchContent_Declare(Catch2
         GIT_REPOSITORY https://github.com/catchorg/Catch2.git
         GIT_TAG        v2.13.7)
